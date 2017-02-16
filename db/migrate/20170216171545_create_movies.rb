fa1bfb81c7fd8da999143fd0e53adb90d9648443
yellowpad <zayneabraham1@gmail.com>
2017-02-16 12:59:06 -0500

    Movie dates

---
+++ db/migrate/20170216171545_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[5.0]
+  def change
+  	create_table :movies do |t|
+  		t.string 	:title 
+  		t.datetime 	:release_date 
+  		t.string 	:director 
+  		t.string 	:lead
+  		t.boolean	:in_theaters
+
+  		t.timestamps
+  	end
+  end
+end
